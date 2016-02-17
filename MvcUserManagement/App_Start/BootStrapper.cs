6fcaf54ced8f0bab421c59f3740477d7a474d86f
raghav.rosberg <ragahv.rosberg@live.com>
2016-02-17 16:43:06 +0530

    Updated to the fastest IOC container 'LightInject'

--- MvcUserManagement/App_Start/BootStrapper.cs
+++ MvcUserManagement/App_Start/BootStrapper.cs
@@ -1,4 +1,4 @@
-﻿using Autofac;
+﻿using System.Data.Entity;
 using MvcDataManager;
 using MvcUserManagement.data;
 using MvcUserManagement.data.EmployeeRespository;
@@ -8,11 +8,12 @@ using System.Linq;
 using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
-using Autofac;
-using Autofac.Integration.Mvc;
 using System.Reflection;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity;
+
+using LightInject;
+
 
 namespace MvcUserManagement.App_Start
 {
@@ -25,18 +26,12 @@ namespace MvcUserManagement.App_Start
 
         private static void SetAutoFacContainer()
         {
-            var dbContext = new MvcUserManagementDbContext();
-            var builder = new ContainerBuilder();
-            builder.RegisterControllers(Assembly.GetExecutingAssembly());
-
-            builder.Register(c => new UnitOfWork(dbContext))
-                .As<IUnitOfWork>().InstancePerLifetimeScope();
-            builder.RegisterGeneric(typeof(RepositoryBase<>)).As(typeof(IRepository<>));
-            
-            builder.RegisterType<EmployeeRepository>().As<IEmployeeRepository>().InstancePerLifetimeScope();
-
-            IContainer container = builder.Build();
-            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));    
+            var container = new ServiceContainer();
+            container.RegisterControllers(Assembly.GetExecutingAssembly());
+            container.Register<IUnitOfWork>((factory) => new UnitOfWork(new MvcUserManagementDbContext()));
+            container.Register(typeof(IRepository<>), typeof(RepositoryBase<>));
+            container.Register<IEmployeeRepository, EmployeeRepository>(new PerRequestLifeTime());
+            container.EnableMvc();
         }
     }
 }
